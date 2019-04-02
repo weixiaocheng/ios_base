7fca801e88675601009bb477a4d9a07dcf324dbc
liuliang <454493675@qq.com>
2019-04-02 09:52:11 +0800

    添加自动布局 和 一些常用个宏定义

---
+++ IOSBASE/IOSBASE/Define/BaseSetHeader.h
@@ -0,0 +1,13 @@
+//
+//  BaseSetHeader.h
+//  IOSBASE
+//
+//  Created by apple on 2019/4/2.
+//  Copyright © 2019 corzen. All rights reserved.
+//
+
+#ifndef BaseSetHeader_h
+#define BaseSetHeader_h
+
+
+#endif /* BaseSetHeader_h */
