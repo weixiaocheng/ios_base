652816a828abd416c9801ccc8ad7bad3ad141f4b
liuliang <454493675@qq.com>
2019-04-04 15:47:26 +0800

    封装自定义的  tablview 成功

---
+++ IOSBASE/IOSBASE/Utils/TableView.h
@@ -0,0 +1,17 @@
+//
+//  TableView.h
+//  IOSBASE
+//
+//  Created by apple on 2019/4/4.
+//  Copyright © 2019 corzen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IOSBASE/IOSBASE/Utils/TableView.m
@@ -0,0 +1,21 @@
+//
+//  TableView.m
+//  IOSBASE
+//
+//  Created by apple on 2019/4/4.
+//  Copyright © 2019 corzen. All rights reserved.
+//
+
+#import "TableView.h"
+
+@implementation TableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
